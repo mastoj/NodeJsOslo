5916b8389daad7c9d874ec6914bac64ce5459f7a
Tomas Jansson <tomas.jansson@gmail.com>
2012-05-26 11:04:07 +0000

    some clean up

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var http = require('http');
 var request = require('request');
 
+var port = process.env.PORT || 3000;
+
 var requestHandler = {};
 requestHandler.handleRequest = function(req, res) {
 	request("https://api.github.com/users/glennblock/repos").pipe(res);
 };
 
-http.createServer(requestHandler.handleRequest).listen(3000);
+http.createServer(requestHandler.handleRequest).listen(port);
 console.log('Server listening at port 3000')
